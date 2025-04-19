23bd265a40769899dbcbd5c49d8aced8f77662c6
BhuvanB <bhuvanb1408@gmail.com>
2025-04-19 11:31:23 +0530

    schedule push

--- star.c
+++ star.c
@@ -1,3 +1,57 @@
-//
-// Created by bhuvan on 6/4/25.
-//
+#include "star.h"
+//#include<stdio.h>
+void Getcwd(char *buf[1024], size_t size)
+    {
+      if(NULL == getcwd(buf, size))
+        perror("getcwd failed");
+      }
+
+char *read_line(void)
+    {
+          char *buf;
+          ssize_t bufsize;
+          char *cwd[1024];
+
+          buf = NULL;
+
+          Getcwd(cwd, sizeof(cwd));
+          printf(BLUE" %s "RST WHITE "*>" RST ,cwd);
+
+
+          if(getline(&buf, &bufsize, stdin) == -1)
+          {
+            buf = NULL;
+            if(feof(stdin))
+              printf(RED"[EOF]"RST);
+            else
+              printf(RED"Getline failed"RST);
+
+            printf("\n");
+
+          }
+
+          return buf;
+  }
+
+int main(int argc, char** argv)
+{
+
+  char *line;
+   //TASK: PRINT A BANNER
+    //DONT FORGET
+
+
+
+    while(line = read_line())
+    {
+          line = read_line();
+          printf("%s", line);
+
+       // getting tokens from
+//          pause();
+
+
+      }
+
+    return EXIT_SUCCESS;
+}
--- star.h
+++ star.h
@@ -1,8 +1,17 @@
-//
-// Created by bhuvan on 6/4/25.
-//
 
 #ifndef STAR_H
 #define STAR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define Y  "\033[1;33m"
+#define RED  "\033[1;31m"
+#define GREEN  "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE  "\033[1;34m"
+#define RST "\033[O"
+#define WHITE "\033[1;37m"
 
 #endif //STAR_H
